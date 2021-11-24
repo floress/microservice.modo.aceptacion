8f36e801148d70a09f4cff4a87e42ac9b3171eb0
Sebastián Flores <floress@gmail.com>
2021-11-24 12:19:13 -0300

    removing publish profiles

---
+++ src/Modo.Clients/IMerchantClient.cs
@@ -0,0 +1,13 @@
+﻿namespace Modo.Clients;
+
+public interface IMerchantClient
+{
+    /// <summary>
+    /// Obtener comercio por cuit
+    /// </summary>
+    /// <param name="cuit">Numero de cuit del comercio</param>
+    /// <param name="id">Id del comercio en modo opcional</param>
+    /// <returns></returns>
+    Task<ObtenerComercioResponse> ObtenerComercio(long cuit, long? id);
+    Task<CrearComercioResponse> CrearComercio(CrearComercioRequest request);
+}
--- src/Modo.Clients/MerchantClient.cs
+++ src/Modo.Clients/MerchantClient.cs
@@ -3,18 +3,6 @@ using System.Net.Http.Json;
 
 namespace Modo.Clients;
 
-public interface IMerchantClient
-{
-    /// <summary>
-    /// Obtener comercio por cuit
-    /// </summary>
-    /// <param name="cuit">Numero de cuit del comercio</param>
-    /// <param name="id">Id del comercio en modo opcional</param>
-    /// <returns></returns>
-    Task<ObtenerComercioResponse> ObtenerComercio(long cuit, long? id);
-    Task<CrearComercioResponse> CrearComercio(CrearComercioRequest request);
-}
-
 public class MerchantClient : IMerchantClient
 {
     private readonly HttpClient _client;
